1d673ffbeebf76eefbc9b56bee94fb23697ff150
unknown <ronanlina@gmail.com>
2021-08-08 16:49:48 +0800

    rough homepage. firstname & lastname fields change and migrate

--- appointment/models.py
+++ appointment/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from django.contrib import admin
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
     email = models.EmailField(max_length=200)
 
     def Client_Name(self):
-        return self.first_name + ' ' + self.last_name
+        return self.firstname + ' ' + self.lastname
 
 class Appointment(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
